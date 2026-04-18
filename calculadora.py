4818892ebed3c756a2d23998a22f9ce4d0cdf419
Cristiano Muroni <muronidev@gmail.com>
2026-04-18 01:49:26 -0300

    up, nodes, compile graph

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,28 @@
 from langchain.tools import tool
 from langchain.chat_models import init_chat_model
+from langchain.messages import SystemMessage
+from langchain.messages import AnyMessage
+from langchain.messages import HumanMessage
+from langchain.messages import ToolMessage
+from langgraph.graph import StateGraph, START, END
+from langchain_google_genai import ChatGoogleGenerativeAI
+from IPython.display import Image, display
+from typing_extensions import TypedDict, Annotated
+import operator
+from typing import Literal
+import os
+from dotenv import load_dotenv
 
 
-model = init_chat_model("here ---> model-name-version", temperature=0)
+load_dotenv()
 
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-3.1-flash-latest",    
+    temperature=0,
+    google_api_key=os.getenv("GOOGLE_API_KEY"),
+)
 
 # Define tools
 @tool
@@ -43,3 +62,76 @@ def divide(a: int, b: int) -> float:
 tools = [add, multiply, divide]
 tools_by_name = {tool.name: tool for tool in tools}
 model_with_tools = model.bind_tools(tools)
+
+# Define state
+class MessagesState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+    llm_calls: int
+    
+# Define model node
+def llm_call(state: dict):
+    """LLM decides whether to call a tool or not"""
+
+    return {
+        "messages": [
+            model_with_tools.invoke(
+                [
+                    SystemMessage(
+                        content="You are a helpful assistant tasked with performing arithmetic on a set of inputs."
+                    )
+                ]
+                + state["messages"]
+            )
+        ],
+        "llm_calls": state.get("llm_calls", 0) + 1,
+    }
+    
+# Define tool node
+def tool_node(state: dict):
+    """Performs the tool call"""
+
+    result = []
+    for tool_call in state["messages"][-1].tool_calls:
+        tool = tools_by_name[tool_call["name"]]
+        observation = tool.invoke(tool_call["args"])
+        result.append(ToolMessage(content=observation, tool_call_id=tool_call["id"]))
+    return {"messages": result}
+    
+# Define end logic
+def should_continue(state: MessagesState) -> Literal["tool_node", END]:
+    """Decide if we should continue the loop or stop based upon whether the LLM made a tool call"""
+
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    # If the LLM makes a tool call, then perform an action
+    if last_message.tool_calls:
+        return "tool_node"
+
+    # Otherwise, we stop (reply to the user)
+    return END
+
+# Build and compile the agent
+# Build workflow
+agent_builder = StateGraph(MessagesState)
+
+# Add nodes
+agent_builder.add_node("llm_call", llm_call)
+agent_builder.add_node("tool_node", tool_node)
+
+# Add edges to connect nodes
+agent_builder.add_edge(START, "llm_call")
+agent_builder.add_conditional_edges("llm_call", should_continue, ["tool_node", END])
+agent_builder.add_edge("tool_node", "llm_call")
+
+# Compile the agent
+agent = agent_builder.compile()
+
+# Show the agent
+display(Image(agent.get_graph(xray=True).draw_mermaid_png()))
+
+# Invoke
+messages = [HumanMessage(content="Add 3 and 4.")]
+messages = agent.invoke({"messages": messages})
+for m in messages["messages"]:
+    m.pretty_print()
